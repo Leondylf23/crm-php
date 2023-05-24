864041b0930fd9d77cec3d080be115d311291337
Leondylf23 <Leondylf23@users.noreply.github.com>
2023-05-25 03:09:23 +0700

    fix komplain

--- komplain.php
+++ komplain.php
@@ -49,7 +49,7 @@ require 'cek.php';
                                     <tbody>
 
                                     <?php 
-                                        $ambilsemuadatakomplain = mysqli_query($conn, 'SELECT k.idkomplain, k.nama, k.komplain, DATE_FORMAT(k.tanggal, "%d-%m-%Y") AS tanggal, kk.nama_kategori, k.idkategori, k.solusi, DATE_FORMAT(k.tgl_solusi, "%d-%m-%Y") AS tanggal_sls, k.is_active FROM komplain k INNER JOIN kategori_komplain kk ON k.idkategori = kk.id ORDER BY is_active DESC, k.tanggal DESC');
+                                        $ambilsemuadatakomplain = mysqli_query($conn, 'SELECT k.idkomplain, k.nama, k.komplain, DATE_FORMAT(k.tanggal, "%d-%m-%Y") AS tanggal, kk.nama_kategori, k.idkategori, k.solusi, DATE_FORMAT(k.tgl_solusi, "%Y-%m-%d") AS tgl_solusi, DATE_FORMAT(k.tgl_solusi, "%d-%m-%Y") AS tanggal_sls, k.is_active FROM komplain k INNER JOIN kategori_komplain kk ON k.idkategori = kk.id ORDER BY is_active DESC, k.tanggal DESC');
                                         $i = 1;
                                         while($data=mysqli_fetch_array($ambilsemuadatakomplain)){
                                             $idkomplain = $data['idkomplain'];
@@ -61,6 +61,7 @@ require 'cek.php';
                                             $solusi = $data['solusi'];
                                             $status = $data['is_active'];
                                             $tglsolusi = $data['tanggal_sls'];
+                                            $tglsolusidata = $data['tgl_solusi'];
 
                                             if($status == 1) {
                                                 $status = "Aktif";
@@ -118,7 +119,7 @@ require 'cek.php';
                                                                 <label for="sls">Solusi</label>
                                                             </div>
                                                             <div class="form-floating mb-3">                                                                                
-                                                                <input type="date" name="tglsolusi" class="form-control" value="<?=$tglsolusi;?>" id="tglsls" required>
+                                                                <input type="date" name="tglsolusi" class="form-control" value="<?=$tglsolusidata;?>" id="tglsls" required>
                                                                 <label for="tglsls">Tanggal Selesai Solusi</label>
                                                             </div>
                                                             <div class="form-floating mb-3">                    
